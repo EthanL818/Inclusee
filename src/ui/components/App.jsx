2b9fc437596fcdefb7f76e42040b87a0ef6b35e2
EspeMV <130682738+EspeMV@users.noreply.github.com>
2024-08-04 12:05:46 -0400

    fixed monochromatic scheme colours

--- src/ui/components/App.jsx
+++ src/ui/components/App.jsx
@@ -3,6 +3,7 @@ import "@spectrum-web-components/theme/express/scale-medium.js";
 import "@spectrum-web-components/theme/express/theme-light.js";
 import { jsx } from "@emotion/react";
 import { Theme } from "@swc-react/theme";
+import { useState } from "react";
 import ContrastAnalyzer from "./ContrastAnalyzer";
 import ReadabilityAnalyzer from "./ReadabilityAnalyzer";
 import "./App.css"; // Ensure App.css is imported
@@ -11,7 +12,7 @@ const App = ({ addOnUISdk, sandboxProxy }) => {
   const [analysisResult, setAnalysisResult] = useState(null);
 
   const handleAnalysisResult = (result) => {
-    console.log('Analysis Result in App:', result); // Ensure this logs correctly
+    console.log("Analysis Result in App:", result); // Ensure this logs correctly
     setAnalysisResult(result);
   };
 
